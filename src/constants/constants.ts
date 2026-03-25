f216267081207174257d968e9bea610032cb41b4
GarvitGoyal18902 <garvit18902@gmail.com>
2026-03-25 23:23:18 +0530

    fix for environment deployment

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,1 @@
-// export const API_BASE_URL = 'https://assignment-backend-taupe.vercel.app';
-export const API_BASE_URL = 'http://localhost:5001';
-
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
